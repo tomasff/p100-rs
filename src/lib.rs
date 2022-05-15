dfb88e89526c67f81b21829c8769199cdcdb73ad
Tomás F <tomasff02@gmail.com>
2022-05-15 17:09:08 +0100

    Begin implementation of the P100

--- src/lib.rs
+++ src/lib.rs
@@ -13,3 +13,17 @@ struct P100 {
     ip_address: String,
     terminal_uuid: Uuid,
 }
+
+impl P100 {
+    fn handshake(&self) {
+        todo!()
+    }
+
+    fn login(&self) {
+        todo!()
+    }
+
+    fn set_state(&self, on: bool) {
+        todo!()
+    }
+}
